53e889bba9052e2cc35ce339623412fc1fe93347
unknown <Ifro@GJM-LAB6-07.ifro.local>
2024-11-11 15:50:04 -0400

    alteraçao botao

--- App/Views/header.php
+++ App/Views/header.php
@@ -20,9 +20,9 @@
 
             <!-- Botões alinhados à direita da tela -->
             <div class="position-absolute" style="right: 0;">
-                <a class="btn btn-info mr-3"
+                <a class="btn btn-success mr-3"
                  href="<?=URL?>/usuarios/cadastrar" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
-                <a class="btn btn-info" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
+                <a class="btn btn-success" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
             </div>
         </nav>
     </div>
